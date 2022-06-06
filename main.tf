ca33587c43c261886bd4ade133f7d70fb0602f72
Olamide Oladejo <olamideoladejo@outlook.com>
2022-06-06 18:58:54 +0000

    Second commit - security group,public, ip, serverport

--- main.tf
+++ main.tf
@@ -1,13 +1,44 @@
 provider "aws" {
-   region = "us-east-2" 
+  region = "us-east-2"
 }
 
 
 resource "aws_instance" "example" {
- ami = "ami-0fb653ca2d3203ac1"
- instance_type = "t2.micro"
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
- tags = {
-     Name = "terraform-example"
- }
+  user_data = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p ${var.server_port} &
+EOF
+
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+variable "server_port" {
+  type        = number
+  default      = 8080
+  description = "The port the server will use for HTTP requests"
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public IPaddress of the web server"
 }
